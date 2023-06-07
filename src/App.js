df12a4c4390298557aba86f7b27f6f66de6a5251
Nikhil Tiwary <ntiwary61@gmail.com>
2023-06-07 21:25:02 +0530

    Create videos object

--- src/App.js
+++ src/App.js
@@ -2,31 +2,47 @@ import "./App.css";
 import Video from "./components/Video";
 
 function App() {
-  let thumbnailData = {
-    title: "Cute Cats",
-    channelName: "CatsFoundation",
-    views: "2m",
-    time: "4 months ago",
-  };
+  let videos = [
+    {
+      id: 1,
+      title: "Cute Cats",
+      channelName: "CatsFoundation",
+      views: "2m",
+      time: "4 months ago",
+      verified: true,
+    },
+    {
+      id: 2,
+      title: "Adorable Kitty",
+      channelName: "CatNation",
+      views: "155k",
+      time: "1 month ago",
+    },
+    {
+      id: 3,
+      title: "Cat food",
+      channelName: "Cat-o-cat",
+      views: "222m",
+      time: "4 year ago",
+      verified: true,
+    },
+    {
+      id: 4,
+    },
+  ];
 
   return (
     <div className="app">
       <div className="sectionTitle">Videos</div>
-      <Video
-        title="Cute Cats"
-        channelName="CatsFoundation"
-        views="2m"
-        time="4 months ago"
-        verified={true}
-      ></Video>
-      <Video
-        title="Adorable Kitty"
-        channelName="CatsFoundation"
-        views="325K"
-        time="3 days ago"
-      ></Video>
-      <Video></Video>
-      <Video {...thumbnailData}></Video>
+      {videos.map((video) => (
+        <Video
+          title={video.title}
+          channelName={video.channelName}
+          views={video.views}
+          time={video.time}
+          verified={video.verified}
+        ></Video>
+      ))}
     </div>
   );
 }
