e4a5d1912b0521857de824fd14541362654dae02
Nikhil Tiwary <ntiwary61@gmail.com>
2023-06-09 20:55:09 +0530

    Add key to Video component and refactor video object into a separate data file

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import "./App.css";
 import Video from "./components/Video";
+import videos from "./data/data";
 
 function App() {
-  let videos = [
-    {
-      id: 1,
-      title: "Cute Cats",
-      channelName: "CatsFoundation",
-      views: "2m",
-      time: "4 months ago",
-      verified: true,
-    },
-    {
-      id: 2,
-      title: "Adorable Kitty",
-      channelName: "CatNation",
-      views: "155k",
-      time: "1 month ago",
-    },
-    {
-      id: 3,
-      title: "Cat food",
-      channelName: "Cat-o-cat",
-      views: "222m",
-      time: "4 year ago",
-      verified: true,
-    },
-    {
-      id: 4,
-    },
-  ];
-
   return (
     <div className="app">
       <div className="sectionTitle">Videos</div>
       {videos.map((video) => (
         <Video
+          key={video.id}
           id={video.id}
           title={video.title}
           channelName={video.channelName}
---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+let videos = [
+  {
+    id: 1,
+    title: "Cute Cats",
+    channelName: "CatsFoundation",
+    views: "2m",
+    time: "4 months ago",
+    verified: true,
+  },
+  {
+    id: 2,
+    title: "Adorable Kitty",
+    channelName: "CatNation",
+    views: "155k",
+    time: "1 month ago",
+  },
+  {
+    id: 3,
+    title: "Cat food",
+    channelName: "Cat-o-cat",
+    views: "222m",
+    time: "4 year ago",
+    verified: true,
+  },
+  {
+    id: 4,
+  },
+];
+
+export default videos;
