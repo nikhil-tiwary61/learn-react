f7f98c1b430863b5591aa59205b833a1411200ad
Nikhil Tiwary <ntiwary61@gmail.com>
2023-06-16 13:14:12 +0530

    Add onDoubleClick and onClick eventListeners on button

--- src/components/Chapter4Assignment1.js
+++ src/components/Chapter4Assignment1.js
@@ -1,12 +1,32 @@
-export default function Assignment1() {
+export default function Assignment1({ onMouseOver }) {
+  function handleMouseOver(e) {
+    e.stopPropagation();
+    console.log("Mouse Over");
+  }
+  function handleMouseOut(e) {
+    e.stopPropagation();
+    console.log("Mouse Out");
+  }
+  function handleDoubleClick(e) {
+    e.stopPropagation();
+    alert("Button was double-clicked");
+  }
+  function handleClick(e) {
+    e.stopPropagation();
+    console.log("Button was single clicked");
+  }
   return (
     <>
       <img
         src="https://loremflickr.com/160/180"
         alt="DummyImage"
         className="image"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
       />
-      <button>Button</button>
+      <button onClick={handleClick} onDoubleClick={handleDoubleClick}>
+        Button
+      </button>
       <input type="text" />
     </>
   );
